718c9115b7fc9fd5ba52759ac299e01dfd7f5c8a
moleNULL <94998782+moleNULL@users.noreply.github.com>
2023-02-09 13:31:19 +0200

    Changed folder for WebAPI_UnitTests project and imported Catalog.UnitTests from A-Level eShop_Sample4

---
+++ Catalog/Catalog.UnitTests/Services/CatalogServiceTest.cs
@@ -0,0 +1,99 @@
+using System.Threading;
+using WebAPI_UnitTests.Data;
+using WebAPI_UnitTests.Data.Entities;
+using WebAPI_UnitTests.Models.Dtos;
+using WebAPI_UnitTests.Models.Response;
+using WebAPI_UnitTests.Repositories.Interfaces;
+using WebAPI_UnitTests.Services.Implementations;
+using WebAPI_UnitTests.Services.Interfaces;
+
+namespace Catalog.UnitTests.Services;
+
+public class CatalogServiceTest
+{
+    private readonly ICatalogService _catalogService;
+
+    private readonly Mock<ICatalogItemRepository> _catalogItemRepository;
+    private readonly Mock<IMapper> _mapper;
+    private readonly Mock<IDbContextWrapper<ApplicationDbContext>> _dbContextWrapper;
+    private readonly Mock<ILogger<CatalogService>> _logger;
+
+    public CatalogServiceTest()
+    {
+        _catalogItemRepository = new Mock<ICatalogItemRepository>();
+        _mapper = new Mock<IMapper>();
+        _dbContextWrapper = new Mock<IDbContextWrapper<ApplicationDbContext>>();
+        _logger = new Mock<ILogger<CatalogService>>();
+
+        var dbContextTransaction = new Mock<IDbContextTransaction>();
+        _dbContextWrapper.Setup(s => s.BeginTransactionAsync(CancellationToken.None)).ReturnsAsync(dbContextTransaction.Object);
+
+        _catalogService = new CatalogService(_dbContextWrapper.Object, _logger.Object, _catalogItemRepository.Object, _mapper.Object);
+    }
+
+    [Fact]
+    public async Task GetCatalogItemsAsync_Success()
+    {
+        // arrange
+        var testPageIndex = 0;
+        var testPageSize = 4;
+        var testTotalCount = 12;
+
+        var pagingPaginatedItemsSuccess = new PaginatedItems<CatalogItemEntity>()
+        {
+            Data = new List<CatalogItemEntity>()
+            {
+                new CatalogItemEntity()
+                {
+                    Name = "TestName",
+                },
+            },
+            TotalCount = testTotalCount,
+        };
+
+        var catalogItemSuccess = new CatalogItemEntity()
+        {
+            Name = "TestName"
+        };
+
+        var catalogItemDtoSuccess = new CatalogItemDto()
+        {
+            Name = "TestName"
+        };
+
+        _catalogItemRepository.Setup(s => s.GetByPageAsync(
+            It.Is<int>(i => i == testPageIndex),
+            It.Is<int>(i => i == testPageSize))).ReturnsAsync(pagingPaginatedItemsSuccess);
+
+        /*_mapper.Setup(s => s.Map<CatalogItemDto>(
+            It.Is<CatalogItem>(i => i.Equals(catalogItemSuccess)))).Returns(catalogItemDtoSuccess);*/
+
+        // act
+        var result = await _catalogService.GetCatalogItemsAsync(testPageSize, testPageIndex);
+
+        // assert
+        result.Should().NotBeNull();
+        result?.Data.Should().NotBeNull();
+        result?.Count.Should().Be(testTotalCount);
+        result?.PageIndex.Should().Be(testPageIndex);
+        result?.PageSize.Should().Be(testPageSize);
+    }
+
+    [Fact]
+    public async Task GetCatalogItemsAsync_Failed()
+    {
+        // arrange
+        var testPageIndex = 1000;
+        var testPageSize = 10000;
+
+        _catalogItemRepository.Setup(s => s.GetByPageAsync(
+            It.Is<int>(i => i == testPageIndex),
+            It.Is<int>(i => i == testPageSize))).Returns((Func<PaginatedItemsResponse<CatalogItemDto>>)null!);
+
+        // act
+        var result = await _catalogService.GetCatalogItemsAsync(testPageSize, testPageIndex);
+
+        // assert
+        result.Should().BeNull();
+    }
+}
